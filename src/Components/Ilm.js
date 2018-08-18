2ecafcb060f85f11cebe3eb39dc98c839e4864fa
mikifriki <mihkel.liblikman@gmail.com>
2018-08-18 23:50:16 +0300

    Cleaned up spacing for Ilm.js and added .eslintrc

--- src/Components/Ilm.js
+++ src/Components/Ilm.js
@@ -1,50 +1,51 @@
-import React from "react";
+import React from 'react';
 
-class Ilm extends React.Component{
+class Ilm extends React.Component {
 
-render(){
-    return(
-        <div>
-            <br1/>
 
-            {this.props.Linn && this.props.Riik && 
-                <p class="City">
-                    <i class="fa fa-compass"></i>
-                    Location: { this.props.Linn} { this.props.Riik } 
-                </p>
-            }
+	render() {
+		return (
+			<div>
+				<br1 />
 
-            
+				{
+					this.props.Linn && this.props.Riik &&
+					<p className="City">
+						<i className="fa fa-compass"></i>
+						Location: {this.props.Linn} {this.props.Riik}
+					</p>
+				}
 
-            {this.props.Temperatuur && 
-                 <p class="Temp">
-                    <i class="fa fa-thermometer"></i> 
-                    Temperature: { this.props.Temperatuur }°C
-                 </p>
-            }
 
-            {this.props.Niiskus && 
-                <p class="Humidity">
-                    <i class="fa fa-tint"> </i>
-                    Humidity: { this.props.Niiskus }%
-                </p>
-            }
 
-            { this.props.Selgitus && 
-                <p class="Cloud">
-                    <i class="fa fa-cloud"></i>
-                    Cloudiness: { this.props.Selgitus}
-                </p>
-            }
-            
-            { this.props.Error && <p5>{ this.props.Error }</p5>}
-            
-            
+				{
+					this.props.Temperatuur &&
+					<p className="Temp">
+						<i className="fa fa-thermometer"></i>
+						Temperature: {this.props.Temperatuur}°C
+					</p>
+				}
 
-        </div>
-    );
+				{
+					this.props.Niiskus &&
+					<p className="Humidity">
+						<i className="fa fa-tint"> </i>
+						Humidity: {this.props.Niiskus}%
+					</p>
+				}
 
-    };
+				{
+					this.props.Selgitus &&
+					<p className="Cloud">
+						<i className="fa fa-cloud"></i>
+						Cloudiness: {this.props.Selgitus}
+					</p>
+				}
+
+				{this.props.Error && <p5>{this.props.Error}</p5>}
+			</div>
+		);
+	}
+
 }
-
 export default Ilm;
