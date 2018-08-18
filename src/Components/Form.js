6530717d9928ae212cd90bd5c2afe6f02b1dda0b
The Ranger <ranger@risk.ee>
2018-08-18 19:21:11 +0300

    Fix code formatting

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,21 +3,15 @@ import React from "react";
 
 class Form extends React.Component {
 
-    render(){
-        return(
-          <form onSubmit={this.props.getIlm }>
-            <input type='tekst' name='Linn' placeholder='City...'/>
-            
-            <input type='tekst' name='Riik' placeholder='Country...'/>
-            
-            <button>Show!</button>
-
-          </form> 
-            
-        );
-    }
-
+	render() {
+		return (
+			<form onSubmit={this.props.getIlm}>
+				<input type='tekst' name='Linn' placeholder='City...' />
+				<input type='tekst' name='Riik' placeholder='Country...' />
+				<button>Show!</button>
+			</form>
+		);
+	}
 }
-
 
 export default Form;
