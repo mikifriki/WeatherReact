8110398c98fa370e9ff4d1bfd60b73f9f421b604
mikifriki <mihkel.liblikman@gmail.com>
2018-08-18 23:53:55 +0300

    Cleaned up code, fixed spacing and changed quotes

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 
-const API_KEY =`${process.env.REACT_APP_API_KEY}`
+const API_KEY = `${process.env.REACT_APP_API_KEY}`;
 
 class Form extends React.Component {
 
@@ -10,16 +10,16 @@ class Form extends React.Component {
 		const Linn = e.target.elements.Linn.value;
 		const Riik = e.target.elements.Riik.value;
 		if (!Linn || !Riik) {
-			this.props.weatherError("");
+			this.props.weatherError('');
 			return;
 		}
 
 		const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${Linn},${Riik}&appid=${API_KEY}&units=metric`);
 		const data = await api_call.json();
-		console.log(data);
 
+		
 		if (!data || !data.name) {
-			this.props.weatherError("Invalid response");
+			this.props.weatherError('Please use a valid location');
 			return;
 		}
 
@@ -30,12 +30,13 @@ class Form extends React.Component {
 	render() {
 		return (
 			<form onSubmit={this.getIlm}>
-				<input type='tekst' name='Linn' placeholder='City...' />
-				<input type='tekst' name='Riik' placeholder='Country...' />
+				<input type="tekst" name="Linn" placeholder="City..." />
+				<input type="tekst "name="Riik" placeholder="Country..." />
 				<button>Show!</button>
 			</form>
 		);
 	}
+
 }
 
 export default Form;
