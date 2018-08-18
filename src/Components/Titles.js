b60291832b1847d8cd92c66736b5114c6c79b3ed
mikifriki <mihkel.liblikman@gmail.com>
2018-08-18 23:57:29 +0300

    Removed spacing and changed from class to className

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -1,20 +1,15 @@
-import React from "react";
-
+import React from 'react';
 
 class Titles extends React.Component{
 
-    render(){
-        return(
-            
-         <div>
-            
-            <h2><i class="fa fa-bolt"></i> The Weather</h2>
-            
-            <p>Input your location to see the weather!</p>
-         </div>   
+	render(){
+		return(   
+			<div>
+				<h2><i className="fa fa-bolt"></i> The Weather</h2>
+				<p>Input your location to see the weather!</p>
+			</div>   
+		);
+	}
 
-        );
-    }
 }
-
 export default Titles;
