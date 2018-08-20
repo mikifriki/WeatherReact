3c814bd87d8ef40785132b9e6d42b59b04adb679
mikifriki <mihkel.liblikman@gmail.com>
2018-08-20 15:13:01 +0300

    Fixed Code Style

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-class Titles extends React.Component{
+class Titles extends React.Component {
 
-	render(){
-		return(   
+	render() {
+		return (
 			<div>
 				<h2><i className="fa fa-bolt"></i> The Weather</h2>
 				<p>Input your location to see the weather!</p>
-			</div>   
+			</div>
 		);
 	}
 
